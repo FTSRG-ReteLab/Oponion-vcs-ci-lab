2951a3136878cef0a9a1610330246e8a78de6d7e
Balazs Prehoda <prehoda.balazs@gmail.com>
2019-02-11 08:28:49 -0800

    Write TrainTachograph.java

--- tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
+++ tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
@@ -2,12 +2,22 @@ package hu.bme.mit.train.tachograph;
 
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
+import hu.bme.mit.train.system.TrainSystem;
 
 public class TrainTachograph {
-    Table<String, String, Integer> table
+    Table<Long, Integer, Integer> table
             = HashBasedTable.create();
 
+    TrainSystem system;
+
+    public TrainTachograph(TrainSystem ts) {
+        system = ts;
+    }
+
     public void recordValue(){
-        
+        int referenceSpeed = system.getController().getReferenceSpeed();
+        int position = system.getUser().getJoystickPosition();
+        long time = System.currentTimeMillis();
+        table.put(time, position, referenceSpeed);
     }
 }
