3acbc871e5dede3bdb0b84017694715b92a9f6ff
Balazs Prehoda <prehoda.balazs@gmail.com>
2019-02-11 07:48:09 -0800

    Fix broken build, add JUnit test

---
+++ train-user/src/test/java/hu/bme/mit/train/user/TrainUserTest.java
@@ -0,0 +1,13 @@
+package hu.bme.mit.train.user;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TrainUserTest {
+
+    @Test
+    public void testTrue() {
+        Assert.assertTrue(true);
+    }
+}
